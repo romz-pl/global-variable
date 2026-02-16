ca802605230341c15a7480d900af679972484af8
romz <romz@wp.pl>
2026-02-16 19:02:21 +0100

    thread-safe implementations with mutex

--- global.h
+++ global.h
@@ -1,4 +1,5 @@
 #include <memory>
+#include <mutex>
 #include <stdexcept>
 
 template<typename B>
@@ -16,28 +17,33 @@ public:
 
     template<std::derived_from<B> D = B, typename... Args>
     void init(Args&&... args) {
+        std::lock_guard<std::mutex> lock(mtx);
         if (ptr)
             throw std::runtime_error("global already initialized");
         ptr = std::make_unique<D>(std::forward<Args>(args)...);
     }
 
     void reset() noexcept {
+        std::lock_guard<std::mutex> lock(mtx);
         ptr.reset();
     }
 
     B& get() {
+        std::lock_guard<std::mutex> lock(mtx);
         if (!ptr)
             throw std::runtime_error("global not initialized");
         return *ptr;
     }
 
     const B& get() const {
+        std::lock_guard<std::mutex> lock(mtx);
         if (!ptr)
             throw std::runtime_error("global not initialized");
         return *ptr;
     }
 
     bool exists() const noexcept {
+        std::lock_guard<std::mutex> lock(mtx);
         return ptr != nullptr;
     }
 
@@ -47,4 +53,5 @@ public:
 
 private:
     std::unique_ptr<B> ptr;
+    mutable std::mutex mtx;
 };
